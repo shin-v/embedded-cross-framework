96991a4b00dc100147c4816d5b33fa6b08b8c8cb
Space <158745757+shin-v@users.noreply.github.com>
2024-06-03 12:49:47 +1000

    Added esp32 board cmake (WIP)

---
+++ boards/ESP/freenove_wroveren4r8/board.cmake
@@ -0,0 +1,13 @@
+string(TOUPPER ${board_name} BOARD_NAME_UPPERCASE)
+set(${board_name}_CLASS "ESP32")
+set(${board_name}_MODULE "WROVER")
+set(${board_name}_FAMILY "E")
+set(${board_name}_MODEL "N4R8")
+set(${board_name}_CPUNAME "XTENSA_32BIT_LX6")
+set(${board_name}_CPU "XTENSA_CPU_32BIT_LX6")
+set(${board_name}_DEFINES -D${BOARD_NAME_UPPERCASE} -D${${board_name}_CLASS} -D${${board_name}_CLASS}${${board_name}_MODULE} -D${${board_name}_CLASS}${${board_name}_MODULE}${${board_name}_FAMILY} -D${${board_name}_CLASS}${${board_name}_MODULE}${${board_name}_FAMILY}xx -D${${board_name}_CLASS}${${board_name}_MODULE}${${board_name}_FAMILY}${${board_name}_MODEL})
+
+# Work In Progress
+#set(${board_name}_CFLAGS ${ARM_CPU_CORTEX_M3_FLAGS} ${ARM_CPU_CORTEX_THUMB_INTERWORK_FLAGS} ${ARM_CPU_ABI_SOFT_FLOAT_FLAGS} -ffunction-sections -fdata-sections -fno-common -fmessage-length=0)
+#set(${board_name}_LDFLAGS ${ARM_CPU_CORTEX_M3_FLAGS} ${ARM_CPU_CORTEX_THUMB_INTERWORK_FLAGS} ${ARM_CPU_ABI_SOFT_FLOAT_FLAGS} ${TOOLCHAIN_LINKER_FLAG} ${TOOLCHAIN_LINKER_PREFIX}--gc-sections ${TOOLCHAIN_LINKER_EXTRA_LDFLAGS})
+#set(${board_name}_LIBDIR ${TOOLCHAIN_LIBDIR} ${TOOLCHAIN_LIBGCC_DIR})
